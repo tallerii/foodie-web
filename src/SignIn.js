c62bf733dedecbe723d2efeaa0f517545c5842e5
Damián Muszalski <damianfiuba@gmail.com>
2019-09-16 01:19:01 -0300

    Initial Commit

---
+++ src/SignIn.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="http://taller-de-programacion-2.github.io">
+        Taller de Programación II - 2c2019
+      </Link>{' '}
+    </Typography>
+  );
+}
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: purple[500],
+        light: '#757ce8',
+        main: '#3f50b5',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(./pizza.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                <Typography component="h1" variant="h3">
+                    Foodie
+                </Typography>
+                <Typography component="h4" variant="h5">
+                    Ingresar
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Usuario"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    />
+                    <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Contraseña"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Recordar mi usuario"
+                    />
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    >
+                    Ingresar
+                    </Button>
+                    <Grid container>
+                    <Grid item xs>
+                        <Link href="#" variant="body2">
+                        ¿Olvidó su contraseña?
+                        </Link>
+                    </Grid>
+                    </Grid>
+                    <Box mt={5}>
+                    <Copyright />
+                    </Box>
+                </form>
+                </div>
+            </Grid>
+        </Grid>
+    </ThemeProvider>
+  );
+}
